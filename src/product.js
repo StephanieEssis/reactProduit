79fb1f7e6ab2d4aa9ff63106b520f80fc3e4a0d2
stephanieessis@gmail.com <stephanieessis@gmail.com>
2025-03-24 13:31:12 +0000

    fix:product-sale

--- src/product.js
+++ src/product.js
@@ -1,6 +1,6 @@
 const product = {
     name: "sac a main dame",
-    price: "150000",
+    price: "150.000",
     description: "Un sac à main élégant et pratique, parfait pour accompagner vos tenues au quotidien. Il offre un design raffiné et une capacité idéale pour ranger vos essentiels avec style.",
     image: "https://img.freepik.com/photos-gratuite/belle-sac-main-vert-elegance-mode-luxe_1203-7655.jpg?t=st=1742821344~exp=1742824944~hmac=d5db641057bfcbddf46d7d3f77e14d08e88eb2c7102e1890abc813a4666daa9d&w=740",
 
